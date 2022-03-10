26f55c3ad91327a808913079c0eb3110b84a435a
Matheus <ms.conrado25@hotmail.com>
2022-03-10 19:39:53 -0300

    criando os metodos createdArchetypeInstances e archetypeInstances

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -1,10 +1,12 @@
 class Archetype {
   protected _special: number;
   protected _cost: number;
+  private static _archetypeInstances = 0;
 
   constructor(protected _name: string) {
     this._special = 0;
     this._cost = 0;
+    Archetype.archetypeInstances();
   }
 
   public get name(): string {
@@ -17,6 +19,14 @@ class Archetype {
 
   public get cost(): number {
     return this._cost;
+  }
+
+  public static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  private static archetypeInstances(): void {
+    this._archetypeInstances += 1;
   }
 }
 
