cb8508208e2cb08b000904833c48d3ebdc25ffc3
Matheus <ms.conrado25@hotmail.com>
2022-03-10 15:44:23 -0300

    criando os metodos get para retornar o name e o dexterity

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,3 +1,11 @@
 class Race {
   constructor(private name: string, private dexterity: number) {}
+
+  public get getName(): string {
+    return this.name;
+  }
+
+  public get getDexterity(): number {
+    return this.dexterity;
+  }
 }
