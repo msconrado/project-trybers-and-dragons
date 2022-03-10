f8260fde6d657d9bdf33d02cfc45bd8914cd1243
Matheus <ms.conrado25@hotmail.com>
2022-03-10 19:26:13 -0300

    criando e implementando o constructor

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -1,3 +1,11 @@
 class Archetype {
-  
+  public special: number;
+  public cost: number;
+
+  constructor(public name: string) {
+    this.special = 0;
+    this.cost = 0;
+  }
 }
+
+export default Archetype;
