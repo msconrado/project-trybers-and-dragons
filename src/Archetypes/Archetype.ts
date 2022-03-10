a5bfd712230e5e508a8abb997506a0fa739b110d
Matheus <ms.conrado25@hotmail.com>
2022-03-10 19:30:19 -0300

    criando os metodos get name, special, cost

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -1,10 +1,22 @@
 class Archetype {
-  public special: number;
-  public cost: number;
+  protected _special: number;
+  protected _cost: number;
 
-  constructor(public name: string) {
-    this.special = 0;
-    this.cost = 0;
+  constructor(protected _name: string) {
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get special(): number {
+    return this._special;
+  }
+
+  public get cost(): number {
+    return this._cost;
   }
 }
 
