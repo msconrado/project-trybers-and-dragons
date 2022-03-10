9b61286f868664f3d3d54baead2bd4a329d79e68
Matheus <ms.conrado25@hotmail.com>
2022-03-10 17:15:00 -0300

    criando o metodo static createdRacesInstances, que lança um error e o atributo _racesInstances que ira atualizar a cada vez que for instanciado um corrida

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,5 +1,9 @@
 class Race {
-  constructor(private _name: string, private _dexterity: number) {}
+  private static _racesInstances = 0;
+
+  constructor(private _name: string, private _dexterity: number) {
+    Race.racesInstances();
+  }
 
   public get name(): string {
     return this._name;
@@ -7,6 +11,14 @@ class Race {
 
   public get dexterity(): number {
     return this._dexterity;
+  }
+
+  private static racesInstances(): void {
+    this._racesInstances += 1;
+  }
+
+  public static createdRacesInstances(): number {
+    throw new Error('Not implemented');
   }
 }
 
