7616b384cc8b456c91cf3b197692c6ef268005b7
Matheus <ms.conrado25@hotmail.com>
2022-03-11 01:04:18 -0300

    implementando o constructor

--- src/Character.ts
+++ src/Character.ts
@@ -1,7 +1,9 @@
-import Archetype from './Archetypes';
+import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
 import Fighter from './Fighter';
-import Race from './Races';
+import Race, { Elf } from './Races';
+
+const randomNumber = (): number => Number((Math.random() * 10 + 1).toFixed(0));
 
 class Character implements Fighter {
   private _race: Race;
@@ -12,6 +14,20 @@ class Character implements Fighter {
   private _defense: number;
   private _dexterity: number;
   private _energy: Energy;
+
+  constructor(public name: string) {
+    this._race = new Elf(name, 0);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = randomNumber();
+    this._defense = randomNumber();
+    this._dexterity = this._race.dexterity;
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: randomNumber(),
+    };
+  }
 }
 
 export default Character;
