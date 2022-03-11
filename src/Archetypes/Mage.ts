26dedc4310ed1fe87807e08596c245e2653947eb
Matheus <ms.conrado25@hotmail.com>
2022-03-10 23:46:01 -0300

    implementando os metodos e atributo para o Mage

--- src/Archetypes/Mage.ts
+++ src/Archetypes/Mage.ts
@@ -1,5 +1,27 @@
+import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
-class Mage extends Archetype {}
+class Mage extends Archetype {
+  private _hability: EnergyType;
+  private static _mageInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._hability = 'mana';
+    Mage.mageInstances();
+  }
+
+  private static mageInstances() {
+    this._mageInstances += 1;
+  }
+
+  public static createdArchetypeInstances(): number {
+    return Mage._mageInstances;
+  }
+
+  public get energyType(): EnergyType {
+    return this._hability;
+  }
+}
 
 export default Mage;
