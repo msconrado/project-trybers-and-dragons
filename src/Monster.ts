09da68653ec11d9082277affea096d167cc9c7c5
Matheus <ms.conrado25@hotmail.com>
2022-03-11 09:41:12 -0300

    implementando os private e o constructor

--- src/Monster.ts
+++ src/Monster.ts
@@ -1,5 +1,13 @@
 import { SimpleFighter } from './Fighter';
 
-class Monster implements SimpleFighter {}
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+}
 
 export default Monster;
