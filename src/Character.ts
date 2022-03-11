85dd02b179f0bdc851324719e50fbce0fe52e0e9
Matheus <ms.conrado25@hotmail.com>
2022-03-11 01:42:36 -0300

    implementando o metodo receiveDamage

--- src/Character.ts
+++ src/Character.ts
@@ -2,8 +2,7 @@ import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
 import Fighter from './Fighter';
 import Race, { Elf } from './Races';
-
-const randomNumber = (): number => Number((Math.random() * 10 + 1).toFixed(0));
+import getRandomInt from './utils';
 
 class Character implements Fighter {
   private _race: Race;
@@ -20,13 +19,48 @@ class Character implements Fighter {
     this._archetype = new Mage(name);
     this._maxLifePoints = this._race.maxLifePoints / 2;
     this._lifePoints = this._maxLifePoints;
-    this._strength = randomNumber();
-    this._defense = randomNumber();
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
     this._dexterity = this._race.dexterity;
     this._energy = {
       type_: this._archetype.energyType,
-      amount: randomNumber(),
+      amount: getRandomInt(1, 10),
     };
+  }
+
+  public get race(): Race {
+    return this._race;
+  }
+
+  public get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  public get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  public get strength(): number {
+    return this._strength;
+  }
+
+  public get defense(): number {
+    return this._defense;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  public get energy(): Energy {
+    return this._energy;
+  }
+
+  public receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this.defense;
+    if (damage > 0) this._lifePoints -= damage;
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+    return this._lifePoints;
   }
 }
 
