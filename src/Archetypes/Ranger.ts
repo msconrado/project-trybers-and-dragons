c051676dc8d58a18eb84a8543a3559808a8b4402
Matheus <ms.conrado25@hotmail.com>
2022-03-11 00:01:08 -0300

    implementando os metodos e atributo para o Ranger

--- src/Archetypes/Ranger.ts
+++ src/Archetypes/Ranger.ts
@@ -1,5 +1,27 @@
+import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
-class Ranger extends Archetype {}
+class Ranger extends Archetype {
+  private _hability: EnergyType;
+  private static _rangerCount = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._hability = 'mana';
+    Ranger.rangerCount();
+  }
+
+  private static rangerCount() {
+    this._rangerCount += 1;
+  }
+
+  public static createdArchetypeInstances(): number {
+    return Ranger._rangerCount;
+  }
+
+  public get energyType(): EnergyType {
+    return this._hability;
+  }
+}
 
 export default Ranger;
