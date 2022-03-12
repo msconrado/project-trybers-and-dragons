e3c7b5680e236516fcd4a73ac56d215e471ee755
Matheus <ms.conrado25@hotmail.com>
2022-03-12 09:01:21 -0300

    criando o constructor do PVE

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -1,5 +1,14 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Monster from '../Monster';
 import Battle from './Battle';
 
-class PVE extends Battle {}
+class PVE extends Battle {
+  constructor(
+    public player: Fighter,
+    public monstersOrPlayer: Monster[] | Fighter | SimpleFighter,
+  ) {
+    super(player);
+  }
+}
 
 export default PVE;
