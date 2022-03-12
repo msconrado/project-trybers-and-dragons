89376add4a89629fce9c33e9c835108447fbe151
Matheus <ms.conrado25@hotmail.com>
2022-03-12 10:48:00 -0300

    executando o levelUp do player1 varias vezes

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,7 @@ const player1 = new Character('Goku');
 const player2 = new Character('Vegeta');
 
 const player3 = new Character('Piccolo');
+
+for (let index = 0; index < 10; index += 1) {
+  player1.levelUp();
+}
