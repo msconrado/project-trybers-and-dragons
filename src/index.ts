8808cf7600d73ac387bf4d146d97d9abd2ef4cd7
Matheus <ms.conrado25@hotmail.com>
2022-03-12 10:43:57 -0300

    instanciando os objetos player1, 2 e 3

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Character from './Character';
 
-const player1: Character = {};
+const player1 = new Character('Goku');
 
-const player2: Character = {};
+const player2 = new Character('Vegeta');
 
-const player3: Character = {};
+const player3 = new Character('Piccolo');
