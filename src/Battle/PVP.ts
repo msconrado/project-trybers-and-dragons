b5e7cd2c83a9b5e0ff8f30479bea78f632fc5770
Matheus <ms.conrado25@hotmail.com>
2022-03-12 08:53:25 -0300

    fazendo a sobrescrita do metodo fight

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -2,11 +2,16 @@ import Fighter from '../Fighter';
 import Battle from './Battle';
 
 class PVP extends Battle {
-  constructor(
-    public player1: Fighter,
-    public player2: Fighter,
-  ) {
+  constructor(public player1: Fighter, public player2: Fighter) {
     super(player1);
+  }
+
+  fight(): number {
+    this.player1.attack(this.player2);
+
+    this.player2.attack(this.player1);
+
+    return super.fight();
   }
 }
 
