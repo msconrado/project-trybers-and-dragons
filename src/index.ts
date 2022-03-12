6e4908df7cf177bc6fa9ce9dcc14a6245d3e4c12
Matheus <ms.conrado25@hotmail.com>
2022-03-12 11:01:01 -0300

    instanciando o objeto PVP passando player2 e player3

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
+import { PVP } from './Battle';
 import Character from './Character';
 import Dragon from './Dragon';
 import Monster from './Monster';
 
 const player1 = new Character('Goku');
-
 const player2 = new Character('Vegeta');
-
 const player3 = new Character('Piccolo');
 
 for (let index = 0; index < 10; index += 1) {
@@ -14,5 +13,7 @@ for (let index = 0; index < 10; index += 1) {
 
 const monster1 = new Monster();
 const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
 
 export { player1, player2, player3, monster1, monster2 };
