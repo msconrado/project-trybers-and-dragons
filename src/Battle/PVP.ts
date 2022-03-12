7f9888ab4663bd21b7c90f2655330ef9b1f0cb0e
Matheus <ms.conrado25@hotmail.com>
2022-03-12 08:37:06 -0300

    criando o constructor do PVP e importando o PVP para o index

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,5 +1,13 @@
+import Fighter from '../Fighter';
 import Battle from './Battle';
 
-class PVP extends Battle {}
+class PVP extends Battle {
+  constructor(
+    public player1: Fighter,
+    public player2: Fighter,
+  ) {
+    super(player1);
+  }
+}
 
 export default PVP;
