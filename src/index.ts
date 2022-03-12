a976fe6777a691e40c951541feb87c69d39be79a
Matheus <ms.conrado25@hotmail.com>
2022-03-12 10:57:06 -0300

    instanciando o objeto monster2 como Dragoncd /home/matheus/trybe/projetos/sd-014-a-project-trybers-and-dragons

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Character from './Character';
 import Monster from './Monster';
+import Dragon from './Dragon';
 
 const player1 = new Character('Goku');
 
@@ -12,6 +13,6 @@ for (let index = 0; index < 10; index += 1) {
 }
 
 const monster1 = new Monster();
-const monster2 = new Monster();
+const monster2 = new Dragon();
 
-export { player1, player2, player3 };
+export { player1, player2, player3, monster1, monster2 };
